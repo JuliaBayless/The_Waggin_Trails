ac6eeeead817a79931780124ac5ae21719131ef3
Julia Bayless <julia.allistair@gmail.com>
2021-11-15 13:35:33 -0600

    registration working

--- database.sql
+++ database.sql
@@ -4,12 +4,13 @@ CREATE TABLE "user" (
 	"id" serial NOT NULL,
 	"username" varchar(255) NOT NULL UNIQUE,
 	"password" varchar(255) NOT NULL,
-	"access_level" int NOT NULL,
+	"access_level" int DEFAULT 0,
 	"image" varchar(400),
 	CONSTRAINT "user_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
 
 
 CREATE TABLE "favorites-junction-table" (
@@ -22,16 +23,19 @@ CREATE TABLE "favorites-junction-table" (
 );
 
 
+
 CREATE TABLE "dog_parks" (
 	"id" serial NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"location" varchar(255) NOT NULL UNIQUE,
 	"description" varchar(400) NOT NULL,
 	"image" varchar(400) NOT NULL,
+	"isFav" boolean DEFAULT false,
 	CONSTRAINT "dog_parks_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
 
 
 CREATE TABLE "tags" (
@@ -42,6 +46,8 @@ CREATE TABLE "tags" (
   OIDS=FALSE
 );
 
+
+
 CREATE TABLE "dog_park_tags" (
 	"id" serial NOT NULL,
 	"dog_park_id" int NOT NULL,
@@ -51,6 +57,7 @@ CREATE TABLE "dog_park_tags" (
   OIDS=FALSE
 );
 
+DROP TABLE "user";
 
 CREATE TABLE "ratings" (
 	"id" serial NOT NULL,
@@ -64,14 +71,14 @@ CREATE TABLE "ratings" (
 );
 
 
-ALTER TABLE "favorites-junction-table" ADD CONSTRAINT "favorites-junction-table_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
-ALTER TABLE "favorites-junction-table" ADD CONSTRAINT "favorites-junction-table_fk1" FOREIGN KEY ("dog_park_id") REFERENCES "dog_parks"("id");
+
 
 ALTER TABLE "dog_park_tags" ADD CONSTRAINT "dog_park_tags_fk0" FOREIGN KEY ("dog_park_id") REFERENCES "dog_parks"("id");
 ALTER TABLE "dog_park_tags" ADD CONSTRAINT "dog_park_tags_fk1" FOREIGN KEY ("tag_id") REFERENCES "tags"("id");
 
 ALTER TABLE "ratings" ADD CONSTRAINT "ratings_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 ALTER TABLE "ratings" ADD CONSTRAINT "ratings_fk1" FOREIGN KEY ("dog_park_id") REFERENCES "dog_parks"("id");
+
 
 INSERT INTO "tags" ("tag")
 VALUES
@@ -95,6 +102,7 @@ INSERT INTO "dog_parks" ("name", "location", "description", "image")
 VALUES
 ('Minnehaha Dog Park', '5399 Minnehaha Park Dr S, Minneapolis, MN 55417', 'A 6.6-acre riverfront recreation area featuring trails where dogs with required permits can roam.', 'https://www.minneapolisparks.org/wp-content/uploads/2018/11/Minnehaha_Dog_Park_pups2.jpg'),
 ('Lake of the Isles Dog Park', '2845 W Lake of the Isles Parkway Minneapolis, MN 55405', 'Dogs are allowed to play off-leash in a designated area of this park, located on Lake of the Isles Parkway.', 'https://photos.bringfido.com/attractions/1/2/8/2821/2821_28186.jpg?size=slide&density=2x');
+
 
 INSERT INTO "dog_park_tags" ("dog_park_id", "tag_id")
 VALUES
