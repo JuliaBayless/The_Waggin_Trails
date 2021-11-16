cf2e753184a089c14a72e1c680838bad70bff217
Julia Bayless <julia.allistair@gmail.com>
2021-11-16 10:46:20 -0600

    store and reducer set up for tags

--- src/redux/reducers/tags.reducer.js
+++ src/redux/reducers/tags.reducer.js
@@ -1,13 +1,19 @@
 import { combineReducers } from 'redux';
 
-
-
-
+const allTags = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_All_TAGS':
+            return action.payload
+        default:
+            return state
+    }
+}
 
 
 
 
 
 export default combineReducers({
-   //reducers go here
-  });
+    //reducers go here
+    allTags
+});
