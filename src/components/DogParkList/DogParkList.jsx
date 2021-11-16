c957d427bdcb81dd8a2687b2c039eb7cafb7252f
Julia Bayless <julia.allistair@gmail.com>
2021-11-16 15:02:07 -0600

    dog park list map finish, now to item

--- src/components/DogParkList/DogParkList.jsx
+++ src/components/DogParkList/DogParkList.jsx
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState, useEffect  } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from "react-router";
+import { Container, Paper } from '@material-ui/core';
+import Grid from '@mui/material/Grid';
+import { positions } from '@mui/system';
+import DogParkItem from '../DogParkItem/DogParkItem'
+
 
 
 export default function DogParkList(props) {
- 
-  const store = useSelector((store) => store);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const DogParkList = useSelector((store) => store.dogParks);
   const [heading, setHeading] = useState('Functional Component');
 
+  useEffect(() => {
+    dispatch({type: 'FETCH_ALL_DOG_PARKS'})
+  }, [])
+
+console.log(DogParkList)
   return (
-    <div>
-      <h2>Dog Park LIST + Item here</h2>
-    </div>
+    <Container>
+                <h1 className="movieListHeader">Movie List</h1>
+                <Grid container justifyContent="center"
+                    sx={{ flexGrow: 1 }} container spacing={4}>
+                    {DogParkList.dogParks?.map(dogPark => {
+                        return (
+                            <Grid item key={dogPark.id} xs={12} sm={6} md={5} lg={4}>
+                                <DogParkItem dogPark={dogPark}/>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </Container>
   );
 }
