f2f11481cd8221e4d8d5c19795de6efa13ababad
Julia Bayless <julia.allistair@gmail.com>
2021-11-16 17:06:35 -0600

    cards are looking functional on dog park items, need to add genres

--- src/components/DogParkItem/DogParkItem.jsx
+++ src/components/DogParkItem/DogParkItem.jsx
@@ -1,20 +1,24 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
-import { useEffect, useState } from 'react';
-import { Container, Paper, Box, makeStyles } from '@material-ui/core';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import { Container, Box } from '@material-ui/core';
+import { CardActionArea } from '@mui/material';
+import { pink } from '@mui/material/colors';
 import PetsIcon from '@mui/icons-material/Pets';
+import Typography from '@mui/material/Typography';
 
-
-
-
-
-
+// const useStyles = makeStyles(() => ({
+//   image:{
+//       height: "150px",
+//   },
+// }))
 
 export default function DogParkItem({ dogPark }) {
-
+  // const { image } = useStyles();
   // const store = useSelector((store) => store);
   const [heading, setHeading] = useState('Functional Component');
 
@@ -28,37 +32,61 @@ export default function DogParkItem({ dogPark }) {
 
   return (
 
-    <Paper elevation={3} sx={{ flexGrow: 1 }} sx={{ height: 400, width: 250 }}>
-      <Grid container spacing={2} sx={{ height: 400, width: 250 }}>
+      <Card sx={{ maxWidth: 400 }}
+        elevation={6}
+        onClick={() => { handleSubmitDetails(dogPark) }}
+        sx={{ height: 400, width: 300 }}>
+        <CardActionArea>
+        <CardHeader
+            title={dogPark.name} 
+            subheader={dogPark.location}
+            action={<PetsIcon /> }/>
+        <Box display='flex' flexGrow={1}>
+          </Box>
+          <CardMedia
+            component="img"
+            sx={{ height: 150, width: 300 }}
+            image={dogPark.image_url}
+            alt={dogPark.name} />
+          <CardContent>
+      Tags here
+      </CardContent>
+        </CardActionArea>
+      </Card>
+
+
+
+    // <Paper elevation={3} sx={{ flexGrow: 1 }} sx={{ height: 400, width: 250 }}>
+    //   <Grid container spacing={2} sx={{ height: 400, width: 250 }}>
         
-        <Grid item xs={6}>
-          <Typography
-            variant="h6"
-            gutterBottom
-            component="div"
-          >
-            {dogPark.name}
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-        <PetsIcon />
-        </Grid>
-        <Grid item xs={12}>
+    //     <Grid item xs={6}>
+    //       <Typography
+    //         variant="h6"
+    //         gutterBottom
+    //         component="div"
+    //       >
+    //         {dogPark.name}
+    //       </Typography>
+    //     </Grid>
+    //     <Grid item xs={6}>
+    //     <PetsIcon />
+    //     </Grid>
+    //     <Grid item xs={12}>
 
-          <img src={dogPark.image_url} alt={dogPark.name}
-            sx={{ width: 250, maxheight: 200 }} />
-        </Grid>
+    //       <img src={dogPark.image_url} alt={dogPark.name}
+    //         className={image} />
+    //     </Grid>
 
-        <Typography variant="h6"
-          gutterBottom component="div"
-          className={dogPark.location}>
-        </Typography>
-        {/* <Typography variant="h5" gutterBottom component="div">
-            {genres?.map(genre => genre.name).join(',  ')}
-          </Typography> */}
+    //     <Typography variant="h6"
+    //       gutterBottom component="div"
+    //       className={dogPark.location}>
+    //     </Typography>
+    //     {/* <Typography variant="h5" gutterBottom component="div">
+    //         {genres?.map(genre => genre.name).join(',  ')}
+    //       </Typography> */}
 
-      </Grid>
-    </Paper >
+    //   </Grid>
+    // </Paper >
 
 
 
