65a9dc221e2e0995fb2bdb58ab12fd2ff9516199
Julia Bayless <julia.allistair@gmail.com>
2021-11-27 14:02:02 -0600

    top nav bar functional, need styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from "react-router";
 
 import {
@@ -8,19 +8,12 @@ import {
   Toolbar,
   Typography,
   Box,
-  Button,
   IconButton,
-  Switch,
-  FormControlLabel,
-  FormGroup,
   MenuItem,
   Menu,
-  useTheme,
-  useMediaQuery
 } from "@material-ui/core";
-import { positions } from "@mui/system";
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 //import components
 import LogOutButton from '../LogOutButton/LogOutButton';
@@ -35,16 +28,16 @@ export default function Nav() {
   //hooks
   const classes = useStyles();
   const history = useHistory();
+  const dispatch = useDispatch();
 
   //stores
   const user = useSelector((store) => store.user);
 
-  
+
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleChange = (event) => {
     setAuth(event.target.checked);
-    
   };
 
   const handleMenu = (event) => {
@@ -55,11 +48,18 @@ export default function Nav() {
     setAnchorEl(null);
   }
 
+  //log users out
+  const handleLogOut = () => {
+    history.push('/login')
+    dispatch({ type: 'LOGOUT' })
+  }
+
   //send user home
   const handlePushList = () => {
     setAnchorEl(null);
     history.push('/DogParkList')
   };
+
 
   //send user to list
   const handlePushHome = () => {
@@ -67,84 +67,80 @@ export default function Nav() {
     history.push('/home')
   };
 
-//send use to form
+
+  //send use to form
   const handlePushForm = () => {
     setAnchorEl(null);
     history.push('/addParkForm')
   };
 
-  //BreakPOints
-  const theme = useTheme();
-
-
-  const isMatch = useMediaQuery('960px')
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-      <Toolbar>
-       {user.id && <IconButton
-       
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          onClick={handleMenu}
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton> }
-        
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-     
-          The Waggin' Trails
-         
-        </Typography>
-        
-        {user.id === null &&
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        }
-        {user.id && (
-          <div>
-            <IconButton
-              // size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handlePushForm}>Add A Park</MenuItem>
-              <MenuItem onClick={handlePushHome}>Favorites</MenuItem>
-              <MenuItem onClick={handlePushList}>List</MenuItem>
-            </Menu>
-          </div>
-        )}
-      </Toolbar>
-    </AppBar>
-  </Box>
+      <AppBar position="static">
+        <Toolbar>
+          {user.id && <IconButton
+
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenu}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>}
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+
+            The Waggin' Trails
+
+          </Typography>
+
+          {user.id === null &&
+            // If there's no user, show login/registration links
+            <Link className="navLink" to="/login">
+              Login / Register
+            </Link>
+          }
+          {user.id && (
+            <div>
+              <IconButton
+                // size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <LogoutIcon
+                  onClick={handleLogOut} />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handlePushForm}>Add A Park</MenuItem>
+                <MenuItem onClick={handlePushHome}>Favorites</MenuItem>
+                <MenuItem onClick={handlePushList}>List</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
 
 
-    
+
     // <div className="nav">
     //   <Link to="/home">
     //     <h2 className="nav-title">The Waggin' Trails</h2>
